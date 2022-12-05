68b41ee87d2c4db60d46f728fa5a65aea6b957f3
Jennifer Allan <jenallan@students.hackleyschool.org>
2022-12-05 09:02:22 -0500

    Initial Commit

---
+++ App Demo/App_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_DemoApp.swift
+//  App Demo
+//
+//  Created by Jennifer Allan on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct App_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
